d0336a2ea83f4767416e94a492a48b7fbc57f8c5
George Osondu <browngeorge752@gmail.com>
2022-11-30 04:23:09 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,8 @@ def fizzbuzz():
         if num % 3 == 0:
             print("Fizz", end=" ")
         elif num % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         elif num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end=" ")
         else:
             print("{:d}".format(num), end=" ")
-    print("")
