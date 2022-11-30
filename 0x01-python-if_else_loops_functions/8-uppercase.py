cecee5393d3b01cdf001c242a4723487be9f9609
George Osondu <browngeorge752@gmail.com>
2022-11-30 03:08:47 +0000

    upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+def uppercase(str):
+    up = ""
+    for  c in str:
+        if ord(c) > 96 and ord(c) < 123:
+            up += chr(ord(c) - 32)
+        else:
+            up += c
+    print("{}".format(up))
