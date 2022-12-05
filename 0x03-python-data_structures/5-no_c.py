e29dbe0042f89706eb1d79f2a1ec133c3443b6ff
George Osondu <browngeorge752@gmail.com>
2022-12-05 22:30:50 +0000

    Replace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,4 +3,5 @@ def no_c(my_string):
     if not my_string:
         pass
     else:
-        return ''.join([char if char not in 'cC' else '' for char in my_string])
+        text = ''.join([char if char not in 'cC' else '' for char in my_string])
+        return text
