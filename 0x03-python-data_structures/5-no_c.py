998bf3ce755c22d46b3172acfc04dbd697f68609
George Osondu <browngeorge752@gmail.com>
2022-12-05 22:10:42 +0000

    Replace

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        pass
+    else:
+        replace = ''
+        for char in my_string:
+            if char == 'c' or char == 'C':
+                char == ""
+            replace += char
+        return replace
