1bd9212184e1906155e4aec12a10a43c5035fb08
George Osondu <browngeorge752@gmail.com>
2022-12-06 00:47:50 +0000

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         pass
     else:
-        arr = ''
         for i in matrix:
             for j in i:
                 if j == i[-1]:
