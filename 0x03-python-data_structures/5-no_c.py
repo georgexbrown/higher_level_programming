dbebd1366ba41a34ee12bb80466c3e56a18c1450
George Osondu <browngeorge752@gmail.com>
2022-12-06 02:24:40 +0000

    Replace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     else:
         replace = ""
         for char in my_string:
-            if char != 'c' or char != 'C':
+            if char != 'c' and char != 'C':
                 replace += char
         return replace
