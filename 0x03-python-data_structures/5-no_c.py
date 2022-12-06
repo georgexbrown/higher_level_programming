2415cf5bd05d6aa76f975192e08f0f4188ba548d
George Osondu <browngeorge752@gmail.com>
2022-12-06 02:26:15 +0000

    Replace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        pass
-    else:
         replace = ""
         for char in my_string:
             if char != 'c' and char != 'C':
