3177018ed1dceaed61b07a725ac64c629f236452
George Osondu <browngeorge752@gmail.com>
2022-12-07 02:17:48 +0000

    palindrome

--- 0x03-python-data_structures/main.c
+++ 0x03-python-data_structures/main.c
@@ -74,7 +74,7 @@ int main(void)
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 500);
+    add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 50);
