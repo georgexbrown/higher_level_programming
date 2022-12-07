89d2f66258c14df4786e70eb0cfca47a24449d2a
George Osondu <browngeorge752@gmail.com>
2022-12-07 01:59:03 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,51 @@
+include "lists.h"
+
+/**
+ * is_palindrome - function that checks if a singly linked list is palindrome
+ * @head: pointer to the list (head node)
+ *
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *first = *head, *second = *head;
+	listint_t *temp = *head, *current = NULL;
+
+	*first, *second, *temp = *head;
+	if (!(*head) || !((*head)->next))
+		return (1);
+
+	while (1)
+	{
+		first = first->next->next;
+		if (!first)
+		{
+			current = second->next;
+			break;
+		}
+		if (!first->next)
+		{
+			current = second->next->next;
+			break;
+		}
+		second = second->next;
+	}
+
+	reverse_list(&current);
+
+	while (current && temp)
+	{
+		if (temp->n == current->n)
+		{
+			current = current->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!current)
+		return (1);
+
+	return (0);
+}
---
+++ 0x03-python-data_structures/reverse_list.c
@@ -0,0 +1,21 @@
+include "lists.h"
+
+/**
+ * reverse_list - function that reverses the singly linked list
+ * @head: pointer to the list (head node)
+ *
+ * Return: pointer to the first node (head node)
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL, *current = *head;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
