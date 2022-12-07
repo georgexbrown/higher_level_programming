e364a4628b24735a27173fa07713f4e0e8bafc4a
George Osondu <browngeorge752@gmail.com>
2022-12-07 02:09:01 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,26 @@
 include "lists.h"
 
 /**
+ * reverse_list - function that reverses the singly linked list
+ * @head: pointer to the list (head node)
+ *
+ * Return: pointer to the first node (head node)
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL, *current = *head;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
  * is_palindrome - function that checks if a singly linked list is palindrome
  * @head: pointer to the list (head node)
  *
