60d481049420f55e658332d1344b582c7cb3fde0
George Osondu <browngeorge752@gmail.com>
2022-12-21 04:05:41 +0000

    bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,7 +10,7 @@ class MagicClass:
         """ writing another docstring """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
@@ -19,4 +19,4 @@ class MagicClass:
 
     def circumference(self):
         """such docstring"""
-        return 2 * math.pi * self.
+        return 2 * math.pi * self.__radius
