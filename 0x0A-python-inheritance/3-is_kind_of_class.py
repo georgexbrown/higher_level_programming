5385d3bd77e6f3a4c577f63d19f1f2e911f2321b
George Osondu <browngeorge752@gmail.com>
2023-01-10 03:22:04 +0000

    isinstance class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Defines a function that determines is object is an instance of a class that inherited
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True/False to confirm is an instance or not
+    Args:
+        obj: object to check
+        a_class: class to be compared with
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
