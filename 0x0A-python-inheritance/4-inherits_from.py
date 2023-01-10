53f2e4adb9eb617d20fd1d69536523db6e7065d7
George Osondu <browngeorge752@gmail.com>
2023-01-10 03:35:15 +0000

    inherited class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Defines a function that determines is object is an instance of
+    a class that inherited
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns boolean to confirm is an instance
+    Args:
+        obj: object to check
+        a_class: class to be compared with
+    """
+
+    # if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if type(obj) != a_class:
+        return True
+    return False
