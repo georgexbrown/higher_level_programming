884e5efd54a3ed6219e91722570895accb9b9802
George Osondu <browngeorge752@gmail.com>
2023-01-10 21:42:20 +0000

    square again

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Importing super class Rectangle"""
+Rectangle = __import__(9-rectangle).Rectangle
+
+
+"""Defining the sub class Square to inherit Rectangle"""
+
+
+class Square(Rectangle):
+    """Module instantiation with size"""
+
+    def __init__(self, size):
+        """Validating and privatizing size
+        Args:
+            size: both length and width of square
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
