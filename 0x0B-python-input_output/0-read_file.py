0eabb40de35d7c4e39aaba2c5fcabe8bdcdae229
George Osondu <browngeorge752@gmail.com>
2023-01-10 23:42:17 +0000

    open and read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """Defines function that reads a text file"""
+
+
 def read_file(filename=""):
 
     """Returns content of file read to stdout
@@ -9,4 +11,4 @@ def read_file(filename=""):
     """
     with open(filename, encoding="utf-8") as myFile:
         content = myFile.read()
-        print(content)
+        print(content, end="")
