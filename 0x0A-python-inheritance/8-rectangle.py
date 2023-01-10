ecd7b173c9c8d45873d882293f2676307290f19f
George Osondu <browngeorge752@gmail.com>
2023-01-10 05:07:30 +0000

    base geometry 2

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,22 +2,7 @@
 
 """Defines a class that raises an  exception"""
 
-
-class BaseGeometry():
-
-    """Raises an exception to indicate not implemented"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    """Raise exceptions at validation error"""
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 """Defines a sub class"""
 
