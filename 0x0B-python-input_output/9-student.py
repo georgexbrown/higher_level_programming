6f9856188b406e25dcd4dfbf86439ac329f027aa
George Osondu <browngeorge752@gmail.com>
2023-01-11 02:11:26 +0000

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Defines a class Student"""
+
+
+class Student:
+    """Instantiation with first_name, last_name and age"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name: the first name of object
+            last_name: the last name of object
+            age: the age of object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Defines a function that returns dictionary desc for JSON of obj"""
+
+    def class_to_json(obj):
+        """Returns dictionary decsription with simple data structure for
+            JSON serialization of an object
+        Args:
+            obj: instance of a class
+        """
+
+        return obj.__dict__
