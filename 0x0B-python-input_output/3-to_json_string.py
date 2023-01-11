5f908674bd4f0c3d417821b2513fbbb32505531f
George Osondu <browngeorge752@gmail.com>
2023-01-11 00:14:35 +0000

    dumps

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import json
+
+"""Defines function that returns JSON rep of object"""
+
+
+def to_json_string(my_obj):
+
+    """Returns JSON representation of object (string)
+    Args:
+        my_obj: object to view its json string rep
+    """
+    stringRep = json.dumps(my_obj)
+    return stringRep
