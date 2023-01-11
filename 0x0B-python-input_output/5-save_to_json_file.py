688d431b4da76e9cadc5bb32d7d17eaeac09b2b5
George Osondu <browngeorge752@gmail.com>
2023-01-11 00:44:31 +0000

    dump to file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines function that writes object to file via JSON string repr"""
+import json
+
+
+def save_to_json(my_obj, filename):
+
+    """Writes object to a text file using JSON string representation
+    Args:
+        my_obj: object to serialize to file
+        filename: text file to be witten
+    """
+    with open(filename, mode='w', encoding="utf-8") as myFile:
+        json.dump(my_obj, myFile)
