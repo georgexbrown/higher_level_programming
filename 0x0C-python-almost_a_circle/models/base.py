aa11a0a6424433c7adb5e581f1f4b3d01ea5517f
George Osondu <browngeorge752@gmail.com>
2023-01-17 03:04:54 +0000

    JSON shit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,27 @@ class Base:
         if self.id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON serialization of list of dict
+        Args:
+            list_dictionaries (list): a list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Returns JSON serialization of list of dict
+        Args:
+            list_objs (list): a list of inherited base instances
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as jsonFile:
+            if list_objs is None:
+                jsonFile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonFile.write(Base.to_json_string(list_dicts))
