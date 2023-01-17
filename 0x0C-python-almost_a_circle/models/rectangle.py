143e4b0afc807de3c1d20972af1b25018e47f87d
George Osondu <browngeorge752@gmail.com>
2023-01-17 02:39:04 +0000

    dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -146,3 +146,13 @@ class Rectangle(Base):
                     self.__x = value
                 elif key == "y":
                     self.__y = value
+
+    def to_dictionary(self):
+        """Return the dictionary representation of Rectangle"""
+        return {
+            "id": self.id,
+            "width": self.__width,
+            "height": self.__height,
+            "x": self.__x,
+            "y": self.__y
+        }
